93ee676d23c0d00056f58610678b907d394c1676
Seth Reid <seth.reid@xero.com>
2020-05-03 13:27:35 +1200

    fixed seo issue by adding into config

--- gatsby-config.js
+++ gatsby-config.js
@@ -61,6 +61,7 @@ module.exports = {
         icon: `${__dirname}/src/assets/icons/site-logo.svg`,
       },
     },
+    `gatsby-plugin-react-helmet`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
